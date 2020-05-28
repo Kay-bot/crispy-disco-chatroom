4841376d7e89951fac8953830916b7b67143c564
KAY S <KS.KWANRUAN@GMAIL.com>
2020-05-28 15:24:13 +1000

    chat logic done

--- scripts/chat.js
+++ scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
     this.room = room;
     this.username = username;
     this.chats = db.collection("chats");
+    this.unsub;
   }
   async addChat(message) {
     //   format a chat object
@@ -23,7 +24,7 @@ class Chatroom {
     return response;
   }
   getChats(callback) {
-    this.chats
+    this.unsub = this.chats
       .where("room", "==", this.room)
       .orderBy("created_at")
       .onSnapshot((snapshot) => {
@@ -35,6 +36,16 @@ class Chatroom {
         });
       });
   }
+  updateName(username) {
+    this.username = username;
+  }
+  updateRoom(room) {
+    this.room = room;
+    console.log("room updated");
+    if (this.unsub) {
+      this.unsub();
+    }
+  }
 }
 
 const chatroom = new Chatroom("general", "yoda");
@@ -42,3 +53,12 @@ const chatroom = new Chatroom("general", "yoda");
 chatroom.getChats((data) => {
   console.log(data);
 });
+
+setTimeout(() => {
+  chatroom.updateRoom("gaming");
+  chatroom.updateName("yoshi");
+  chatroom.getChats((data) => {
+    console.log(data);
+  });
+  chatroom.addChat("hello");
+}, 3000);
